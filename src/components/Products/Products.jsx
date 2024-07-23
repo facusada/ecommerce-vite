4350f69a3cce6c3bc633363067643e79b7022d10
facusada <facundosadava1@gmail.com>
2024-07-23 18:49:47 -0300

    Show all products, card items

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,9 +1,45 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import './Products.sass'
 
-const Products = () => (
-  <div>
-    <h1>Products</h1>
-  </div>
-);
+const Products = () => {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const allProducts = async () => {
+      try {
+        setLoading(true);
+        const response = await fetch('https://fakestoreapi.com/products');
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        const data = await response.json();
+        setProducts(data);
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    allProducts();
+  }, []);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error}</p>;
+
+  return (
+    <div className="products-container">
+      {products.map(product => (
+        <div key={product.id} className="card">
+          <img src={product.image} alt={product.title} />
+          <h2>{product.title}</h2>
+          <p className="price">${product.price}</p>
+        </div>
+      ))}
+    </div>
+  )
+};
 
 export default Products;
