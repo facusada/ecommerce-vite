6283c98ae3ea1a56a9437db37641d857aca0a074
facusada <facundosadava1@gmail.com>
2024-07-27 23:47:09 -0300

    Testing changes with modals, implementing Material UI

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,6 +1,8 @@
-import React, { useEffect, useState } from 'react';
-import ReactModal from 'react-modal';
+import React, { useEffect, useState, useRef } from 'react';
 import { ClipLoader } from 'react-spinners';
+import { Modal, Box, Typography, Button, IconButton, Card, CardActions, CardContent, CardMedia } from '@mui/material';
+import { Add, Remove } from '@mui/icons-material';
+
 
 import { fetchAllProducts } from '../../services/Products.js'
 import Pagination from '../Pagination/Pagination.jsx';
@@ -14,6 +16,31 @@ const Products = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [currentPage, setCurrentPage] = useState(1);
   const [productsPerPage] = useState(10);
+  const [count, setCount] = useState(1);
+
+  const handleIncrement = () => {
+    setCount(prevCount => prevCount + 1);
+    onChange(count + 1);
+  };
+
+  const handleDecrement = () => {
+    if (count > 1) {
+      setCount(prevCount => prevCount - 1);
+      onChange(count - 1);
+    }
+  };
+
+  const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+  };
 
   useEffect(() => {
     const getAllProducts = async () => {
@@ -54,11 +81,30 @@ const Products = () => {
     <div>
       <div className="products-container">
         {currentProducts.map(product => (
-          <div key={product.id} className="card" onClick={() => handleCardClick(product)}>
-            <img src={product.image} alt={product.title} />
-            <h2>{product.title}</h2>
-            <p className="price">${product.price}</p>
-          </div>
+          <Card sx={{ maxWidth: 345 }}>
+            <CardMedia
+              component="img"
+              height="140"
+              image={product.image}
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                {product.title}
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                Price: ${product.price}
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Button size="small">Share</Button>
+              <Button size="small">Learn More</Button>
+            </CardActions>
+          </Card>
+          // <div key={product.id} className="card" onClick={() => handleCardClick(product)}>
+          //   <img src={product.image} alt={product.title} />
+          //   <h2>{product.title}</h2>
+          //   <p className="price">${product.price}</p>
+          // </div>
         ))}
       </div>
 
@@ -69,7 +115,72 @@ const Products = () => {
         currentPage={currentPage}
       />
 
-      <ReactModal
+      { selectedProduct &&
+      <Modal
+        open={isModalOpen}
+        onClose={handleCloseModal}
+        aria-labelledby="modal-title"
+        aria-describedby="modal-description"
+      >
+        <Box sx={style}>
+          <img src={selectedProduct.image} alt={selectedProduct.title} style={{ width: '100%', height: 'auto' }} />
+          <br />
+          <Typography id="modal-title" variant="h6" component="h2">
+            {selectedProduct.title}
+          </Typography>
+          <Typography id="modal-description" sx={{ mt: 2 }}>
+            {selectedProduct.description}
+          </Typography>
+          <Typography variant="h6" sx={{ mt: 2 }}>
+            Price: ${selectedProduct.price}
+          </Typography>
+          <Box display="flex" alignItems="center">
+            <IconButton onClick={handleDecrement} disabled={count <= 1}>
+              <Remove />
+            </IconButton>
+            <Typography variant="h6" component="span" mx={2}>
+              {count}
+            </Typography>
+            <IconButton onClick={handleIncrement}>
+              <Add />
+            </IconButton>
+          </Box>
+          <Button onClick={handleCloseModal} sx={{ mt: 2 }}>Close</Button>
+        </Box>
+      </Modal> }
+
+      {/* {selectedProduct &&
+      <Dialog
+        open={isModalOpen}
+        onClose={handleCloseModal}
+        scroll={'paper'}
+        aria-labelledby="scroll-dialog-title"
+        aria-describedby="scroll-dialog-description"
+        className='dialog-selected-product'
+      >
+        <Card sx={{ maxWidth: 400 }}>
+        <CardMedia
+          component="img"
+          height="500"
+          src={selectedProduct.image}
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            Lizard
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Lizards are a widespread group of squamate reptiles, with over 6,000
+            species, ranging across all continents except Antarctica
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button size="small">Share</Button>
+          <Button size="small">Learn More</Button>
+        </CardActions>
+      </Card>
+      </Dialog>} */}
+
+      {/* <ReactModal
         isOpen={isModalOpen}
         onRequestClose={handleCloseModal}
         contentLabel="Product Detail"
@@ -86,7 +197,7 @@ const Products = () => {
             <p className="price">${selectedProduct.price}</p>
           </div>
         )}
-      </ReactModal>
+      </ReactModal> */}
     </div>
   );
 };
